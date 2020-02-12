8ae5b33b0ec874b715bbeb15f763bdd82e2aa838
Jason Anderson <jasonanderson@uchicago.edu>
2020-02-12 15:34:01 -0600

    Allow setting interface for domain admin ks client
    By default, this will use the admin interface because that is the
    default in Keystone client. Users may want to override this, and in fact
    we do, because we write Heat users to a different Keystone server
    entirely and the admin/internal endpoints are not available to us.

--- heat/engine/clients/os/keystone/heat_keystoneclient.py
+++ heat/engine/clients/os/keystone/heat_keystoneclient.py
@@ -162,7 +162,8 @@ class KsClientWrapper(object):
                 session=self.session,
                 auth=self.domain_admin_auth,
                 connect_retries=cfg.CONF.client_retry_limit,
-                region_name=self.region_name_for_domain_admin)
+                region_name=self.region_name_for_domain_admin,
+                interface=cfg.CONF.interface_for_domain_admin)
 
         return self._domain_admin_client
 
